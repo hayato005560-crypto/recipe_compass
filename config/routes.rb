d5a6378b001856e4ad5a8900ca03d1eda978ed32
hayato005560-crypto <hayato005560@gmail.com>
2026-08-04 13:26:47 +0000

    認証基盤とルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  root "homes#top"
+  resource :session, only: [:new, :create, :destroy]
+  post "session/guest", to: "sessions#guest", as: :guest_session
+  #パスワード再設定時に有効化
+  #resources :passwords, param: :token
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :recipes do
+    resources :comments, only: [:create, :destroy]
+  end
+
+  namespace :admin do
+    root "homes#top"
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :recipes, only: [:index, :show, :destroy] do
+      resources :comments, only: [:index, :destroy]
+    end
+    resources :purposes, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -9,6 +27,5 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+
 end
---
+++ db/migrate/20260804044516_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[8.1]
+  def change
+    create_table :users do |t|
+      t.string :email_address, null: false
+      t.string :password_digest, null: false
+      t.string :name, null: false
+      t.text :introduction
+      t.integer :role, null: false, default: 0
+      t.boolean :is_guest, null: false, default: false
+      t.boolean :is_active, null: false, default: true
+
+      t.timestamps
+    end
+    add_index :users, :email_address, unique: true
+  end
+end
