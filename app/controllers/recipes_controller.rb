954605d7df833e912977358519def65f9451a72e
hayato005560-crypto <hayato005560@gmail.com>
2026-08-07 07:30:17 +0000

    レシピCRUDと画像投稿機能を実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,50 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Current.user.recipes.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe, notice: "レシピを投稿しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @recipe = Current.user.recipes.find(params[:id])
+  end
+
+  def update
+    @recipe = Current.user.recipes.find(params[:id])
+
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: "レシピを更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end 
+
+  def destroy
+    @recipe = Current.user.recipes.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :body, :cooking_time, :ingredients, :steps, :image, purpose_ids: [])
+  end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_purposes, dependent: :destroy
   has_many :purposes, through: :recipe_purposes
+  has_one_attached :image
 
   validates :title, :ingredients, :steps, :cooking_time, presence: true
   validates :cooking_time, numericality: { only_integer: true, greater_than: 0 }
