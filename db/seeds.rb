5faba0f42b6d082ee0228260a4190d88aa193d2e
hayato005560-crypto <hayato005560@gmail.com>
2026-08-12 06:09:53 +0000

    テストデータをseedに追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,17 @@ purpose_names = ["ダイエット", "筋肉増量", "時短", "節約"]
 purpose_names.each do |name|
   Purpose.find_or_create_by!(name: name)
 end
+
+test_user = User.find_or_create_by!(email_address: "test@gmail.com") do |user|
+  user.name = "test"
+  user.password = "test"
+end
+
+test_recipe = test_user.recipes.find_or_create_by!(title: "テストタイトル") do |recipe|
+  recipe.steps = "テスト工程"
+  recipe.ingredients = "テスト材料"
+  recipe.cooking_time = 1
+end
+
+test_purpose = Purpose.find_by!(name: "ダイエット")
+test_recipe.purposes << test_purpose unless test_recipe.purposes.include?(test_purpose)
